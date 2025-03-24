7ad8d632412528b9af92871ee7741d5b0acd80c2
gdelgado2 <gdelgado@itba.edu.ar>
2025-03-24 20:53:30 -0300

    Building shared memory functions

--- src/utils/sh_memory.c
+++ src/utils/sh_memory.c
@@ -7,76 +7,87 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "/Users/motegui/Desktop/SO-TP1/src/include/sh_memory.h"
+#include "include/sh_memory.h"
 
 
-void *createSHM(char *name, size_t size) {
+typedef struct{
+    char *name;
+    size_t size;
+    sem_t sem;
+}shm_t;
+
+
+shm_t *create_shm(char *name) {
     int fd;
     fd = shm_open(name, O_RDWR | O_CREAT, 0666); // mode solo para crearla
     if (fd == -1) {
-        perror("shm_open");
+        perror("Error: shm_open");
         exit(EXIT_FAILURE);
     }
 
     // Solo para crearla
-    if (-1 == ftruncate(fd, size)) {
-        perror("ftruncate");
+    if (ftruncate(fd, sizeof(shm_t)) == -1) {
+        perror("Error: ftruncate");
         exit(EXIT_FAILURE);
     }
 
-    void *p = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
-    if (p == MAP_FAILED) {
-        perror("mmap");
+    shm_t *shm_p = mmap(NULL, sizeof(shm_t), PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+    if (shm_p == MAP_FAILED) {
+        perror("Error: mmap");
         exit(EXIT_FAILURE);
     }
 
-    return p;
+    //le asignamos el nombre al puntero
+    strcpy(shm_p->name, name);
+
+    //inicializacion de semaforo con valor 0
+    if(sem_init(&shm_p->sem,1,0) == -1){
+        perror("Error: sem_init");
+        exit(EXIT_FAILURE);
+    }
+
+    return shm_p;
 }
 
-int main(void) {
-    // int x;
-    // int *p = &x;
-    // int *p = (int *) malloc(sizeof(int));
-    // if (!p) exit(EXIT_FAILURE);
+void delete_shm(shm_t *p){
 
-    int *p = (int *) createSHM("/my_shm", sizeof(int));
-    *p = 5;
-
-    pid_t pid;
-
-    sem_t *print_needed = (sem_t *) createSHM("/print_needed", sizeof(sem_t));
-    if (-1 == sem_init(print_needed, 1, 0)) {
-        perror("sem_init");
+    if(sem_destroy(&p->sem) == -1){
+        perror("Error sem_destroy");
         exit(EXIT_FAILURE);
     }
 
-    sem_t *print_done = (sem_t *) createSHM("/print_done", sizeof(sem_t));
-    if (-1 == sem_init(print_done, 1, 0)) {
-        perror("sem_init");
+    if(munmap(p, sizeof(*p)) == -1){
+        perror("Error: munmap");
         exit(EXIT_FAILURE);
     }
 
-    pid = fork();
-    switch (pid) {
-        case -1:
-            perror("fork");
-            exit(EXIT_FAILURE);
+    if(shm_unlink(p->name) == -1){
+        perror("Error: shm_unlink");
+        exit(EXIT_FAILURE);
+    }
+}
 
-        case 0:
-            for (int i = 0; i < 3; i++) {
-                sem_wait(print_needed);
-                printf(" p: %d\n", *p);
-                sem_post(print_done);
-            }
-            exit(EXIT_SUCCESS);
 
-        default:
-            for (int i = 0; i < 3; i++) {
-                *p = i;
-                printf("p: %d\n", i);
-                sem_post(print_needed);
-                sem_wait(print_done);
-            }
-            exit(EXIT_SUCCESS);
+shm_t * connect_shm(const char *shm_name){
+    int fd = shm_open(shm_name, O_RDWR, 0);
+    if(fd == -1){
+        perror("Error: shm_open in connect_shm");
+        exit(EXIT_FAILURE);
+    }
+
+    shm_t * shm_p = mmap(NULL, sizeof(*shm_p), PROT_READ|PROT_WRITE, MAP_SHARED,fd, 0);
+
+    if(shm_p == MAP_FAILED){
+        perror("Error: mmap in connect_shm");
+        exit(EXIT_FAILURE);
+    }
+    
+    return shm_p;
+}
+
+void close_shm(shm_t * shm_p){
+    if(munmap(shm_p,sizeof(*shm_p)) == -1){
+        perror("Error: munmap in close");
+        exit(EXIT_FAILURE);
     }
 }
